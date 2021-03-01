8b1a7d59a6a3b46c4c7ff234d1697a4bb171e668
alonsyrc <alonso.rdz.c@gmail.com>
2021-03-01 00:05:03 -0700

    sprites dialogos

---
+++ Assets/Scripts/DialogosJuicio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="PersonajeDialogos", menuName ="Dialogo")]
+public class DialogosJuicio : ScriptableObject
+{
+    public enum TrueVeredict { Guilty, Inocent};
+    public TrueVeredict trueVeredict;
+    public string NameOfVictim;
+    public string NameOfSuspect;
+    public string CriminalOfRecord;
+    public string BriefOfCharge;
+    public string BriefOfDate;
+    public string BriefOfFacts;
+    public string DialogoOfFijo;
+    public int indexPregunta;
+    public List<string> Questions = new List<string>();
+    public List<string> Answers = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
