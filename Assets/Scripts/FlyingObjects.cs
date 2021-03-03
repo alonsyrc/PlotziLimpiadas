3a5616250105e9621efbd54907b79e2201238a14
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-03-03 01:14:27 -0500

    Questions and flying Object

---
+++ Assets/Scripts/FlyingObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlyingObjects : MonoBehaviour
+{
+    public float radious;
+    public float timeToMove=3f;
+    Vector3 randDirection;
+    Vector3 initPos;
+
+    private void Start()
+    {
+        initPos = transform.position;
+        randDirection.x = Random.Range(-radious,radious);
+        randDirection.y = Random.Range(-radious,radious);
+    }
+
+    public void RestartPosition()
+    {
+        transform.position = initPos;
+    }
+
+    public void MovePos()
+    {
+        transform.DOMove(randDirection,timeToMove).OnComplete(()=>{
+            MovePos();    
+        });
+    }
+}
---
+++ Assets/Scripts/QuestionSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionSpawner : MonoBehaviour
+{
+    ///suscribe evento
+    ///instancia pregunta
+    ///hace click
+    ///Profit
+    ///
+
+    public delegate void QuestionSpawnning();
+    public static event QuestionSpawnning QuestionAnswered;
+
+    public GameObject QuestionPop;
+    public TMPro.TextMeshProUGUI questionText;
+
+    public void SpawnQuestion(string question,DialogosJuicio CaseReport=null/*, QuestionSpawnning functionReturn*/)
+    {
+        
+        questionText.text = question;
+        QuestionPop.GetComponentInChildren<Button>().onClick.AddListener(() => {
+
+            QuestionAnswered.Invoke();
+            TweenOut();
+        }
+        ) ;
+        TweenIN();
+    }
+
+    void TweenIN()
+    {
+
+    }
+
+    void TweenOut()
+    {
+
+    }
+}
