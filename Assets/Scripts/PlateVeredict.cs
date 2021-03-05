9fcb6c1496bc54430c9c4ff9caacdf24370a73d9
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-03-04 23:00:25 -0500

    Vereditct

---
+++ Assets/Scripts/PlateVeredict.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateVeredict : MonoBehaviour
+{
+    public enum VeredictType {NotGuilty,Guilty}
+    public VeredictType veredictType;
+
+    private void OnMouseDown()
+    {
+        if (FindObjectOfType<VeredictHammer>().isDrag)
+        {
+            GiveVeredict();
+            //sonida tac tac
+        }
+    }
+
+    public void GiveVeredict()
+    {
+        switch (veredictType)
+        {
+            case VeredictType.Guilty:
+
+                break;
+            case VeredictType.NotGuilty:
+
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/VeredictHammer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VeredictHammer : MonoBehaviour
+{
+    Sprite hammerDown;
+    public bool isDrag;
+    public Vector3 initPos;
+
+    public void RestartPosition()
+    {
+        isDrag = false;
+        transform.position = initPos;
+    }
+
+    private void OnMouseDown()
+    {
+        ///llevarse el hammer
+        isDrag = !isDrag;
+        Cursor.visible = !isDrag;
+    }
+
+    private void Update()
+    {
+        if (isDrag)
+        {
+            transform.position = new Vector3(
+                Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+
+                Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
+                0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("plate"))
+        {
+            
+        }
+    }
+}
