babad259f9c788062fd2f8aeec0da9fdc550d05b
Daniel Cortes <ryuuvg@gmail.com>
2021-03-05 00:32:58 -0500

    Arte de accesibilidad

---
+++ Assets/Scripts/SopitafyUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class SopitafyUI : MonoBehaviour
+{
+    TextMeshProUGUI txtVolumenPrc;
+    public Slider sliderVolumen;
+    float volPrc;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        txtVolumenPrc = GameObject.Find("txtVolumenPrc").GetComponent<TextMeshProUGUI>();
+        sliderVolumen = GetComponent<Slider>();
+        sliderVolumen.onValueChanged.AddListener(delegate { ChangeVolumen(sliderVolumen.value); });
+        //sopitafy = FindObjectOfType<Sopitafy>();
+        sliderVolumen.value = 1.0f;
+    }
+
+    // cuando se le de click al boton de play pause sopitafy
+    
+    public void ChangeVolumen(float vol)
+    {
+        //sopitafy.ChangeVolume(vol);
+        volPrc = vol * 100;
+        txtVolumenPrc.text = ($"{Mathf.Round(volPrc).ToString()} %");
+    }
+}
